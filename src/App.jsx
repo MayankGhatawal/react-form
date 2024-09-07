d6e01156349d594dbb6dc61f50b502ebb3620681
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-09-07 18:19:18 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { useForm, SubmitHandler } from "react-hook-form"
+import { useForm } from "react-hook-form"
 import './App.css'
 
 function App() {
   const{register, handleSumbit} = useForm();
-
+  const onSubmit = (d) =>{
+    alert(JSON.stringify(d));
+  }
   return (
     <>
         <div className="main">
           <form className='main2' onSubmit={}>
-            <label for="name">First Name:</label><br />
-            <input type="text" id="fname" name="fname" required /><br />
-            <label for="name">Last Name:</label><br />
-            <input type="text" id="lname" name="lname" required /><br />
-            <label for="email">Email:</label><br />
-            <input type="email" id="email" name="email" required /><br />
+            <label for="name">First Name:</label>
+            <input type="text" id="fname" name="fname" required />
+            <label for="name">Last Name:</label>
+            <input type="text" id="lname" name="lname" required />
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required />
             <input type="submit" value="Submit" />
           </form>
         </div>

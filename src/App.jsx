1c31a33e5f096052623790c797e1ce646c1e3a07
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-09-07 18:12:34 +0530

    main

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  useForm()
+
+  return (
+    <>
+        <div className="main">
+          <form className='main2' onSubmit={}>
+            <label for="name">First Name:</label><br />
+            <input type="text" id="fname" name="fname" required /><br />
+            <label for="name">Last Name:</label><br />
+            <input type="text" id="lname" name="lname" required /><br />
+            <label for="email">Email:</label><br />
+            <input type="email" id="email" name="email" required /><br />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+    </>
+  )
+}
+
+export default App
